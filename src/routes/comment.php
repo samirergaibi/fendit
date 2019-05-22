103606850677099c4774b231ded98c847310d975
payamesh <payam.eshtiagh@gmail.com>
2019-05-22 15:35:15 +0200

    created endpoints for comments v1

---
+++ src/routes/comment.php
@@ -0,0 +1,55 @@
+<?php
+
+return function($app){
+    $app->post('/api/comment', function($req, $res){
+
+        $comment = new Comment($this->db);
+        $user = new User($this->db);
+        $createdBy = $user->getUser($_SESSION['username']);
+        $createdBy = $createdBy['userID'];
+        $data = $req->getParsedBody();
+
+       $comment->createComment($entryID, $data['content'], $createdBy ); 
+
+
+    });
+
+    $app->put('/api/comment', function($req,$res){
+
+        $data = $req->getParsedBody();
+        $comment = new Comment($this->db);
+
+
+        $comment->editComment($data['content'], $commentID);
+
+        return $res->withJson([
+            $message => 'Edit success!'
+            ]);
+
+    });
+
+    $app->delete('/api/comment', function($req, $res){
+        $comment = new Comment($this->db);
+
+
+        $comment->deleteComment($commentID);
+
+        return $res->withJson([
+          $message =>  'Comment successfully deleted'
+        ]);
+
+
+    });
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
