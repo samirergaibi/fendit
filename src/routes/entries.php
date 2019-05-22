1662a031ab20ff3a518965281d9a3b5f757531f8
tobbros <fluffisen21@hotmail.com>
2019-05-22 15:40:02 +0200

    added entries endpoints

--- src/routes/entries.php
+++ src/routes/entries.php
@@ -3,13 +3,40 @@
 
     return function($app){
 
-        $app->get("/entries", function($req, $resp){
+        $app->get("/api/entries", function($req, $resp){
             $entry = new Entry($this->db);
-
             return $resp->withJson($entry->getLatestEntries());
         });
 
+        $app->get("/api/entries/{userID}",function($req,$resp,$args){
+            $entry = new Entry($this->db);
+            $userID = $args['userID'];
+            return $resp->withJson($entry->getUserEntries($userID));
+        });
+        $app->post("/api/entry/{userID}", function($req,$resp,$args){
+            $entry = new Entry($this->db);
+            $userID = $args['userID'];
+            $data = $req->getParsedBody();
+            $entry->createEntry($data['title'],$data['content'],$userID);
+         });
+         
+         $app->delete("/api/entry/{entryID}",function($req,$resp,$args){
+            $entry = new Entry($this->db);
+            $entryID = $args['entryID'];
+            $entry->deleteEntry($entryID);
+         });
+
+         $app->put("/api/entry/{entryID}",function($req,$resp,$args){
+           $entry = new Entry($this->db);
+           $entryID = $args['entryID'];
+           $data = $req->getParsedBody();
+           $entry->updateEntry($data['title'],$data['content'],$entryID);
+          
+         });
+
     }
+
+
 
     
 
